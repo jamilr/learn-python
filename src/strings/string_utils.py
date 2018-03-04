bbf2c055f48c6434c4f13f6608011863af609937
Jamil Rzayev <jamil.rzayev@gmail.com>
2018-03-04 18:15:41 +0000

    String utils

--- src/strings/string_utils.py
+++ src/strings/string_utils.py
@@ -34,7 +34,40 @@ def reverser_words_in_sentence(s):
             start = i + 1
     return ''.join(ch)
 
+
+# longest sub-string with at most k distinct characters
+def longest_substring_with_at_most_k_distinct_distinct_characters(s, k):
+    if s is None or len(s) == 0:
+        return 0
+    n = len(s)
+    start = 0
+    max_len = 0
+    max_len_end = 0
+    char_dict = {}
+    for i in range(n):
+        sh = s[i]
+        char_dict[sh] = i
+        if len(char_dict) > k:
+            j = n - 1
+            vv = list(char_dict.values())
+            vv_size = len(vv)
+            for l in range(0, vv_size):
+                if vv[l] < j:
+                    j = vv[l]
+            ssh = s[j]
+            del char_dict[ssh]
+            start = j + 1
+        if max_len < (i - start + 1):
+            max_len = i - start + 1
+            max_len_end = i
+    return s[max_len_end - max_len + 1:max_len_end + 1]
+
+
 if __name__ == '__main__':
     ss = 'I am working at home'
     print('Reversed string  - {}'.format(reverse(ss)))
     print('Reversed words in string  - {}'.format(reverser_words_in_sentence(ss)))
+    ss = 'aaabbaabaabchdsr'
+    print('Longest substring with at most {} distinct characters in a given string {} --> {}'
+          .format(2, ss, longest_substring_with_at_most_k_distinct_distinct_characters(ss, 2)))
+
